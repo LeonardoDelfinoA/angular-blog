154315d2e06561a03e133aafce618b8871617d1f
LeonardoDelfinoA <lleon1997@gmail.com>
2023-10-17 13:33:05 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Novo Homem de Ferro em 3D",
+        "description": "Marvel anuncia novo homem de ferro",
+        "photoCover": "https://observatoriodocinema.uol.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=1400&height=800&format=webp&quality=91&imagick=/wp-content/uploads/2021/02/homem-de-ferro-tony-divulgacao.jpg"
+    },
+    {
+        "id": "2",
+        "title": "Boatos que homem formiga morre em novo filme",
+        "description": "O formigao foi de base",
+        "photoCover": "https://images.universohq.com/2015/07/HomemFormiga_des.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Novo anime de Dragon Ball é anunciado",
+        "description": "Agora os guerreiros Z forma transformados em crianças",
+        "photoCover": "https://proxy.olhardigital.com.br/wp-content/uploads/2023/10/Destaque-Dragon-Ball-Daima.jpg"
+    },
+    {
+        "id": "4",
+        "title": "Netflix prepara um live action de Akira",
+        "description": "Provavelmente vai ficar uma merda",
+        "photoCover": "https://olhardigital.com.br/wp-content/uploads/2019/07/20190705062358.jpg"
+    }
+]
